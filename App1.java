a429e3e118ba9977a96edf04e68fb39f815a3d08
akila543 <akihems666@gmail.com>
2018-07-31 11:35:02 +0530

    Update App1.java

--- App1.java
+++ App1.java
@@ -1,5 +1,10 @@
-public class App1 {
-public static void main(String[] args){
-System.out.println("hello akilaa");
-}
-}
+<?xml version='1.0' encoding='UTF-8'?>
+<hudson.tasks.Mailer_-DescriptorImpl plugin="mailer@1.20">
+  <defaultSuffix>@wipro.com</defaultSuffix>
+  <smtpAuthUsername>avitepa@wipro.com</smtpAuthUsername>
+  <smtpAuthPassword>{AQAAABAAAAAQTiPT9s3K/lgdxLx7knYil1nSP/MI0jZ1+QmojEPRQZc=}</smtpAuthPassword>
+  <smtpHost>smtp.office365.com</smtpHost>
+  <useSsl>false</useSsl>
+  <smtpPort>587</smtpPort>
+  <charset>UTF-8</charset>
+</hudson.tasks.Mailer_-DescriptorImpl>
